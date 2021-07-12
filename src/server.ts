2eb94f66ca2c0bb4d81c161fdc805292b8b5c3d9
Ilannildo Viana <ilannildoviana12@gmail.com>
2021-07-12 18:41:32 -0300

    Add eslint configurations

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response } from 'express'; 
+import express, { Request, Response } from 'express';
 
 const app = express();
-app.get('/', (request: Request, response: Response) => {
-  return response.json({
-    message: "Hello World"
-  });
-})
+app.get('/', (request: Request, response: Response) => response.json({
+  message: 'Hello World',
+}));
 app.listen(3333, () => console.log('Server is running'));
